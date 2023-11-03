f59f5a7b258f7154e0218ccc95bf3bb434edb0f6
crocokyle <me@kyle-dixon.com>
2023-11-03 12:31:56 -0400

    :bug: Fix incorrect charge reporting while drawing from battery

--- driver/__main__.py
+++ driver/__main__.py
@@ -1,10 +1,10 @@
 import datetime
 from pprint import pprint
 from typing import Any
-import time
+
 import pandas as pd
 
-from driver.commands import PowMrCommands
+from driver.commands import PowMrCommands, DerivedCommands
 from driver.connection import PowMrConnection
 
 
@@ -16,6 +16,9 @@ def run_commands(connection) -> dict[str, Any]:
         results[str(command)] = connection.read_register(command, force_update=force_update)
         run_registers.append(command.register)
 
+    for command in DerivedCommands:
+        results[str(command)] = eval(command.expression)
+
     return results
 
 
@@ -24,8 +27,16 @@ def poll(connection: PowMrConnection, poll_count: int = 10):
 
     while len(df) < poll_count:
         results = run_commands(connection)
-        if results['BATTERY_CHARGE_CURRENT_A'] > 0: # Not sure why this is necessary. Draw value jumps up to 6544.
-            results['BATTERY_DRAW_CURRENT_A'] = float(0)
+
+        # Not sure why this is necessary. Draw value jumps up when charging and vice-versa
+        if results['BATTERY_CHARGE_A'] > 0:
+            results['BATTERY_DRAW_A'] = float(0)
+            results['BATTERY_DRAW_W'] = float(0)
+
+        if results['BATTERY_DRAW_A'] > 0:
+            results['BATTERY_CHARGE_A'] = float(0)
+            results['BATTERY_CHARGE_W'] = float(0)
+
         results['timestamp'] = datetime.datetime.now().astimezone(datetime.timezone.utc)
         new = pd.DataFrame(results, index=[results['timestamp']])
         df = pd.concat([df, new])
