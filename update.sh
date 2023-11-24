9f0e6756017d34ced2277802f604192d5ccefbf1
CrocoKyle <me@kyle-dixon.com>
2023-11-24 03:52:21 -0500

    :children_crossing: Remove docker service addition

--- update.sh
+++ update.sh
@@ -6,8 +6,5 @@ git pull
 # Build the RPi images
 sudo docker build . -f driver/Dockerfile -t powmr-driver:rpi
 
-# Enable Docker service on startup
-sudo systemctl enable docker
-
 # Run detached
 sudo docker compose -f docker-compose-rpi.yaml up -d
