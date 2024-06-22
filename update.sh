cd3558069d61eb0566ded074b02c84cb160a7cd5
Crocokyle <me@kyle-dixon.com>
2024-06-22 13:30:04 -0700

    :green_heart: Fix build

--- update.sh
+++ update.sh
@@ -4,7 +4,6 @@ git checkout *
 git pull
 
 # Build the RPi driver images
-sudo docker build . -f drivers/inverter/Dockerfile -t powmr-driver:rpi
 sudo docker build . -f driver/Dockerfile -t powmr-driver:rpi
 
 # Run detached
